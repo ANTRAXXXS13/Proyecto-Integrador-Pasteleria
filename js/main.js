f226c210f827a9f8161397611a022133d1b96cb0
Eri11 <erikaeli11@gmail.com>
2020-03-13 04:03:37 -0700

    Experimento Tienda en Bootsrap

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Primer carruel ~UNFINISHED~
+
+$(".slider-uno")
+    .not(".slick-initialized")
+    .slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true
+    )};
+
+//$(document).ready(function(){
+   // $('.slider-uno').slick();
+//});
